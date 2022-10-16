692eb767857262e3d25ec8f5f92d9fe95ef67335
frantisek.langr <langrfrantisek@gmail.com>
2022-10-16 21:21:48 +0200

    2.4

--- 02/Core/Src/main.c
+++ 02/Core/Src/main.c
@@ -21,6 +21,9 @@
 static volatile uint32_t Tick;
 
 #define LED_TIME_BLINK 300
+#define BUTTON_DEOUBNCE 40
+#define LED_TIME_SHORT 100
+#define LED_TIME_LONG 1000
 
 #if !defined(__SOFT_FP__) && defined(__ARM_FP)
 	#warning "FPU is not inicialized, but the project is compiling for an FPU"
@@ -49,6 +52,40 @@ void blikac(void)
 	}
  }
 
+void tlacitka(void)
+ {
+	static uint32_t debounce1;
+	static uint32_t debounce2;
+	static uint32_t off_time;
+
+	if (Tick > debounce1 + BUTTON_DEOUBNCE) {
+		static uint32_t old_s2;
+		uint32_t new_s2 = GPIOC->IDR & (1<<0);
+
+		if (old_s2 && !new_s2) { // falling edge
+			off_time = Tick + LED_TIME_SHORT;
+			GPIOB->BSRR = (1<<0);
+		}
+		old_s2 = new_s2;
+	}
+
+	if (Tick > debounce2 + BUTTON_DEOUBNCE) {
+		static uint32_t old_s1;
+		uint32_t new_s1 = GPIOC->IDR & (1<<1);
+
+		if (old_s1 && !new_s1) { // falling edge
+			off_time = Tick + LED_TIME_LONG;
+			GPIOA->BSRR = (1<<4);
+		}
+		old_s1 = new_s1;
+	}
+
+	if (Tick > off_time) {
+		GPIOB->BRR = (1<<0);
+		GPIOA->BRR = (1<<4);
+	}
+}
+
 int main(void)
 {
 
@@ -70,6 +107,7 @@ int main(void)
   while (1)
   {
 	  blikac();
+	  tlacitka();
   }
 
 }
