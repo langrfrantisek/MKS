375d418d3ae4b1fef00e0c3b8878870c82583642
frantisek.langr <langrfrantisek@gmail.com>
2022-10-16 20:38:28 +0200

    2.2

--- 02/Core/Src/main.c
+++ 02/Core/Src/main.c
@@ -22,6 +22,15 @@
 	#warning "FPU is not inicialized, but the project is compiling for an FPU"
 #endif
 
+void EXTI0_1_IRQHandler(void)
+ {
+	if (EXTI->PR & EXTI_PR_PR0) { // check line 0 has triggered the IT
+		EXTI->PR |= EXTI_PR_PR0; // clear the pending bit
+		GPIOB->ODR ^= (1<<5); //toggle
+	}
+ }
+
+
 int main(void)
 {
 
@@ -30,6 +39,13 @@ int main(void)
 	GPIOB->MODER |= GPIO_MODER_MODER0_0; // LED2 = PB0, output
 	GPIOC->PUPDR |= GPIO_PUPDR_PUPDR0_0; // S2 = PC0, pullup
 	GPIOC->PUPDR |= GPIO_PUPDR_PUPDR1_0; // S1 = PC1, pullup
+
+	RCC->APB2ENR |= RCC_APB2ENR_SYSCFGEN;
+
+	SYSCFG->EXTICR[0] |= SYSCFG_EXTICR1_EXTI0_PC; // select PC0 for EXTI0
+	EXTI->IMR |= EXTI_IMR_MR0; // mask
+	EXTI->FTSR |= EXTI_FTSR_TR0; // trigger on falling edge
+	NVIC_EnableIRQ(EXTI0_1_IRQn); // enable EXTI0_1
 
   while (1)
   {
